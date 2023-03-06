463565feeb38cfc469c76a020e2f9798af986afc
PeterO76 <41997001+PeterO76@users.noreply.github.com>
2023-03-06 13:44:35 +0100

    making ESAPI available to external developers

--- frontend/api-proxy/src/proxy.php
+++ frontend/api-proxy/src/proxy.php
@@ -1,12 +1,17 @@
 <?php
 ini_set('memory_limit','8G');
+error_reporting(E_ALL ^ E_NOTICE ^ E_WARNING) ;
 include('functions.php');
 include('cache.php');
 
 init();
 
-$request_uri = $_SERVER["REQUEST_URI"];
+$request_uri = str_replace("/esapi","",$_SERVER["REQUEST_URI"]);
+
 $apikey = getallheaders()["APIKEY"];
+if ($apikey == "") {
+        $apikey = getallheaders()["Apikey"];
+}
 $entityBody = json_decode(file_get_contents('php://input'));
 writelog($request_uri);
 writelog($apikey);
@@ -25,11 +30,13 @@ if (isset($entityBody->scroll_id)) {
 
     $query = adaptQuery($entityBody, $permissions->datasets);
 }
-
+writelog($query);
 list($result,$result_code) = getResult($query,$request_uri);
 
 header('Content-Type:application/json');
 http_response_code($result_code);
 print $result;
+
+
 
 ?>
