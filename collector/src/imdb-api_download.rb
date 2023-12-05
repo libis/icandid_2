231ed5cc8b9d7d37229b7305ab39d075c89c4439
PeterO76 <41997001+PeterO76@users.noreply.github.com>
2023-12-05 13:02:15 +0100

    search in smaller timeintervals

--- collector/src/imdb-api_download.rb
+++ collector/src/imdb-api_download.rb
@@ -18,7 +18,7 @@ SOURCE_DIR   = '/source_records/IMDB-API/'
 RECORDS_DIR  = '/records/IMDB-API/'
 
 #############################################################
-TESTING = true
+TESTING = false
 STATUS = "downloading"
 
 COUNT = 100 
@@ -54,6 +54,7 @@ begin
   }
 
   lastchecktime = 0  
+  dayblocks = [[1,7],[8,14],[15,21],[22,27]]
 
   def lastday(year,month)
     lastdaylist = [0,31,28,31,30,31,30,31,31,30,31,30,31]
@@ -91,87 +92,85 @@ begin
 
     for year in START_YEAR..END_YEAR do    
       for month in 1..12 do
-        url_options[:release_date_gte]     = year.to_s + "-" + ("%02d" % month) + "-01"
-        url_options[:release_date_lte]	= year.to_s + "-" + ("%02d" % month) + "-" + lastday(year,month).to_s
+        dayblocks[4] = [28,lastday(year,month)]
+        for dayblock in dayblocks do
+          url_options[:release_date_gte]     = year.to_s + "-" + ("%02d" % month) + "-" + ("%02d" % dayblock[0])
+          url_options[:release_date_lte]	= year.to_s + "-" + ("%02d" % month) + "-" + ("%02d" % dayblock[1])
+#          url_options[:release_date_gte]     = year.to_s + "-" + ("%02d" % month) + "-01"
+#          url_options[:release_date_lte]	= year.to_s + "-" + ("%02d" % month) + "-" + lastday(year,month).to_s
 
-        TITLE_TYPES.each{ |title_type|
+          TITLE_TYPES.each{ |title_type|
 
+            url_options[:title_type] = title_type 
 
-          ## check API usage
+            url = icandid_config::create_url( url: icandid_config.config[:discover_url], query: query, options: url_options)
 
-          if ((Time.now.to_i - lastchecktime) > (10*60))
-            @logger.info ("Checking max allowed usage for today")
-            url = icandid_config::create_url( url: icandid_config.config[:usage_url], query: query, options: url_options)
             data = collector.get_data(url, url_options)
-            @logger.info(data["count"].to_s + " / " + data["maximum"].to_s);
-            while (data["count"] > (0.95*data["maximum"]))
-              @logger.info ("Maximum allowed usage for today exceeded, waiting for a new day ;-)")
-              sleep(10*60)
-              data = collector.get_data(url, url_options)
+            sleep(0.5)
+            collector.retries = 0
+
+            if data.nil?
+                @logger.warn "NO DATA AVAILABLE on this url #{url}"
+                break
             end
-            lastchecktime = Time.now.to_i
-          end
 
-          ## end check API usage
+            unless (data["results"].empty?)
+              @logger.debug ("total record for this query : #{ data["results"].length }")
+              # Expand resultsdata to records with body
+              data["results"].each{ |d|
+              
+                ## check API usage
+
+                if ((Time.now.to_i - lastchecktime) > (10*60))
+                  @logger.info ("Checking max allowed usage for today")
+                  url = icandid_config::create_url( url: icandid_config.config[:usage_url], query: query, options: url_options)
+                  data = collector.get_data(url, url_options)
+                  @logger.info(data["count"].to_s + " / " + data["maximum"].to_s);
+                  while (data["count"] > (0.98*data["maximum"]))
+                    @logger.info ("Maximum allowed usage for today exceeded, waiting for a new day ;-)")
+                    sleep(10*60)
+                    data = collector.get_data(url, url_options)
+                  end
+                  lastchecktime = Time.now.to_i
+                end
+
+                ## end check API usage
+
+                url_options[:movie_id] = d["id"]
+                record_url =  icandid_config::create_record_url( url: icandid_config.config[:record_url], query: query, options: url_options)
+                record_data = collector.get_data(record_url, url_options)
+                sleep(0.5)
+                collector.retries = 0
+                unless record_data.nil? || record_data.empty?
+
+                  review_url =  icandid_config::create_record_url( url: icandid_config.config[:review_url], query: query, options: url_options)
+                  review_data = collector.get_data(review_url, url_options)
+                  sleep(0.5)
+                  unless review_data.nil? || review_data.empty?
+                    record_data["reviews"] = review_data["items"]
+                  end
+
+                  metacriticreview_url =  icandid_config::create_record_url( url: icandid_config.config[:metacriticreview_url], query: query, options: url_options)
+                  metacriticreview_data = collector.get_data(metacriticreview_url, url_options)
+                  sleep(0.5)
+                  unless metacriticreview_data.nil? || metacriticreview_data.empty?
+                    record_data["metacriticreviews"] = metacriticreview_data["items"]
+                  end
 
 
-          url_options[:title_type] = title_type 
-
-          url = icandid_config::create_url( url: icandid_config.config[:discover_url], query: query, options: url_options)
-
-          data = collector.get_data(url, url_options)
-          sleep(2)
+                  record_data = record_data.compact
+                  filename = "#{d["id"]}"
+                  @logger.info("Writing to #{filename}")
+                  output.to_jsonfile(record_data, filename, source_records_dir, true)
+                  exit if TESTING
+                end 
+              }  
+            end
+          }
           collector.retries = 0
-
-          if data.nil?
-              @logger.warn "NO DATA AVAILABLE on this url #{url}"
-              break
-          end
-
-          unless (data["results"].empty?)
-            @logger.debug ("total record for this query : #{ data["results"].length }")
-            # Expand resultsdata to records with body
-            data["results"].each{ |d|
-
-            
-              url_options[:movie_id] = d["id"]
-              record_url =  icandid_config::create_record_url( url: icandid_config.config[:record_url], query: query, options: url_options)
-              record_data = collector.get_data(record_url, url_options)
-              sleep(2)
-              collector.retries = 0
-              unless record_data.nil? || record_data.empty?
-
-                review_url =  icandid_config::create_record_url( url: icandid_config.config[:review_url], query: query, options: url_options)
-                review_data = collector.get_data(review_url, url_options)
-                sleep(2)
-                unless review_data.nil? || review_data.empty?
-                  record_data["reviews"] = review_data["items"]
-                end
-
-                metacriticreview_url =  icandid_config::create_record_url( url: icandid_config.config[:metacriticreview_url], query: query, options: url_options)
-                metacriticreview_data = collector.get_data(metacriticreview_url, url_options)
-                sleep(2)
-                unless metacriticreview_data.nil? || metacriticreview_data.empty?
-                  record_data["metacriticreviews"] = metacriticreview_data["items"]
-                end
-
-
-                record_data = record_data.compact
-                filename = "#{d["id"]}"
-                @logger.info("Writing to #{filename}")
-                output.to_jsonfile(record_data, filename, source_records_dir, true)
-                exit if TESTING
-              end 
-            }  
-          end
-        }
-        collector.retries = 0
+        end
       end
     end
-
-    
-
-
     query[:recent_records][:last_run_update] = start_process
     icandid_config::update_query_config(query: query, index: index)
   end
