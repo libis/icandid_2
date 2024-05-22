7dc52eab64dd56bbbe5bf45d19ef6c8b3c7135f4
Peter O <peter.o@kuleuven.be>
2024-05-22 10:33:47 +0200

    beginning of functionality to check permalinks

---
+++ frontend/front-api/app/Helpers/UuidChecker.php
@@ -0,0 +1,52 @@
+<?php
+namespace App\Helpers;
+use GuzzleHttp\Client as Client;
+use Illuminate\Support\Facades\Log;
+use App\Helpers\Searcher as Searcher;
+use Carbon\Carbon;
+
+class UuidChecker {
+    public function __construct() {
+        $this->url=config('app.elastic_url');
+        $this->searcher = new Searcher((object)["searchtype"=>"sitemap"]);
+        $user = \App\User::where('eppn','uuiduser')
+                ->where('active',1)
+                ->whereDate('startdate', '<=', Carbon::now())
+                ->whereDate('enddate', '>=', Carbon::now())
+                ->firstOrFail();
+        $this->apikey = $user->apikey;
+        $this->c = 0;
+    }    
+
+    public function exec() {
+        $client = new Client(['http_errors' => false]);
+        $data = $this->searcher->first($this->apikey);
+        
+        // loop while there is more data
+        while (count($data) > 0) {
+            foreach($data as $k => $v) {
+                if (isset($v->_source->{'@uuid'})) {
+                    $this->c++;
+                    $url = "https://icandid.libis.be/_/" . $v->_source->{'@uuid'};
+
+                    $response = $client->request('GET', $url); 
+                    //print($response->getStatusCode() . " " . $url . "\n");
+
+                    if ($response->getStatusCode() == 404) {
+                        print($url . "\n");
+                    }
+                    
+                }
+            }
+            $data = $this->searcher->next($this->apikey);
+        }
+
+    }
+   
+
+}
+
+
+
+
+?>
