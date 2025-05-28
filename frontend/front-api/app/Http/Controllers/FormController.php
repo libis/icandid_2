6c707bffa61f8a2807b1a72f7d5da1f40252923c
Peter O <peter.o@kuleuven.be>
2025-05-28 15:08:34 +0200

    new fields user-data, improved email-layout, automatic creating of users

--- frontend/front-api/app/Http/Controllers/FormController.php
+++ frontend/front-api/app/Http/Controllers/FormController.php
@@ -7,6 +7,7 @@ use App\Mail\Mail as Email;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Log;
 use App\Dataset;
+use App\User;
 
 
 
@@ -18,9 +19,9 @@ class FormController extends Controller
         $subject = $request_content->subject;
         foreach ($request_content->formdata as $k => $v) {
             if (is_array($v)) {
-                $body .= $k . " :\t\t" . implode(", ", $v) . "\n";
+                $body .= $this->label($k) . " :\t\t" . implode(", ", $v) . "\n";
             } else {
-                $body .= $k . " :\t\t" . $v . "\n";
+                $body .= $this->label($k) . " :\t\t" . $v . "\n";
             }
         }
         
@@ -32,6 +33,38 @@ class FormController extends Controller
             $body .= "/n/n/n" . print_r(["header"=>$header, "payload"=>$payload], True);
         }    
 
+        $r = (array)$request_content->formdata;
+
+        if (isset($r["loginname"])) {
+
+            // check to see if user already exists
+            // if not, create new user, but leave inactive
+            $user = User::where("eppn",$r["loginname"])->first();
+            if ($user === Null) {
+                $user = new User();
+                $user->eppn = $r["loginname"];
+                $user->firstname = $r["firstname"];
+                $user->lastname = $r["lastname"];
+                $user->email = $r["email"];
+                $user->institution = $r["institution"];
+                $user->active = 0;
+                $user->researchgroup =  $r["researchgroup"];
+                $user->startdate = date("Y-m-d");
+                $user->enddate = "2050-12-31";
+                $user->description = $r["reason"];
+                $user->newsletter = ($r["newsletter"]?1:0);
+                $user->faculty = $r["faculty"];
+                $user->function = $r["function"];
+                $user->language_id = 0;
+                if ($r["language"] == "nl") $user->language_id = 1;
+                if ($r["language"] == "en") $user->language_id = 3;
+                $user->promotor = $r["promotor"];
+                $user->termsofuse = ($r["termsofuse"]?1:0);
+
+                $user->save();
+            }
+        }
+
         Mail::to(config('app.contact'))->send(new Email(nl2br($body),$subject));
     }
 
@@ -39,9 +72,40 @@ class FormController extends Controller
         $this->authorize('search');
         $m = json_decode($request->getContent());
         Mail::to($m->to)->send(new Email(nl2br($m->body),$m->subject));
-
-
-
     }
 
+
+    private function label($k) {
+        $l = array(
+            "email"=>"Email",
+            "institution"=>"Instelling",
+            "institutionname"=>"Naam instelling",
+            "function"=>"Positie",
+            "promotor"=>"Promotor",
+            "faculty" =>"Faculteit",
+            "researchgroup"=>"Onderzoeksgroep",
+            "loginname"=>"Loginnaam",
+            "reason"=>"Reden aanvraag",
+            "from"=>"Vanaf",
+            "until"=>"Tot",
+            "functionality"=>"Functionaliteit",
+            "termsofuse"=>"Servicevoorwaarden aanvaard",
+            "newsletter"=>"Nieuwsbrief",
+            "language"=>"Taal",
+            "firstname"=>"Voornaam",
+            "lastname"=>"Familienaam",
+            "name"=>"Naam",
+            "datasets"=>"Collecties",
+            "provider"=>"Provider",
+            "title"=>"Titel",
+            "searchquery"=>"Zoektermen",
+            "info"=>"Bijkomende informatie"
+        );
+
+        if (isset($l[$k])) {
+            return $l[$k];
+        } else {
+            return $k;
+        }
+    }
 }
